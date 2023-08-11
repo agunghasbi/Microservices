02b086a6c10118ff51efe2e9f1639ed7e7bf7727
Agung Hasbi <agunghasbi13@gmail.com>
2023-08-11 21:35:52 +0700

    Verify Token Middleware

--- api-gateway/routes/handler/users/login.js
+++ api-gateway/routes/handler/users/login.js
@@ -30,6 +30,7 @@ module.exports = async (req, res) => {
 
         return res.json(user.data);
     } catch (error) {
+        console.log(error);
         if (error.code === 'ECONNREFUSED') {
             return res.status(500).json({ status: 'error', message: 'service users unavailable' })
         }
