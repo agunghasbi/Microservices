d8664fdbaf29ca6a39941706dcc3d5211377a587
Agung Hasbi <agunghasbi13@gmail.com>
2023-08-12 18:34:53 +0700

    Get Profile API

--- api-gateway/routes/users.js
+++ api-gateway/routes/users.js
@@ -4,6 +4,7 @@ const router = express.Router();
 const usersHandler = require('./handler/users');
 const verifyToken = require('../middlewares/verifyToken');
 
+router.get('/', verifyToken, usersHandler.getUser);
 router.post('/register', usersHandler.register);
 router.post('/login', usersHandler.login);
 router.put('/', verifyToken, usersHandler.update)
