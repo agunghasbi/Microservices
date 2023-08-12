db3bbe3f3b7d2183ea40844bfa2950b53553848d
Agung Hasbi <agunghasbi13@gmail.com>
2023-08-12 18:39:23 +0700

    User Logout API

--- api-gateway/routes/users.js
+++ api-gateway/routes/users.js
@@ -8,5 +8,6 @@ router.get('/', verifyToken, usersHandler.getUser);
 router.post('/register', usersHandler.register);
 router.post('/login', usersHandler.login);
 router.put('/', verifyToken, usersHandler.update)
+router.post('/logout', verifyToken, usersHandler.logout);
 
 module.exports = router;
