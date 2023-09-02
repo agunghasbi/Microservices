8d6249319e94cac9a8454726425c3122cd92788e
Agung Hasbi <agunghasbi13@gmail.com>
2023-09-02 18:53:15 +0700

    MyCourse Create API

---
+++ service-course/app/Http/Controllers/MyCourseController.php
@@ -0,0 +1,62 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Course;
+use App\Models\MyCourse;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
+
+class MyCourseController extends Controller
+{
+    public function create(Request $request)
+    {
+        $rules = [
+            'course_id' => 'required|integer',
+            'user_id' => 'required|integer'
+        ];
+
+        $data = $request->all();
+        $validate = Validator::make($data, $rules);
+        if ($validate->fails()) {
+            return response()->json([
+                'status' => 'error',
+                'message' => $validate->errors()
+            ], 400);
+        }
+
+        $courseId = $request->input('course_id');
+        $course = Course::find($courseId);
+
+        if (!$course) {
+            return response()->json([
+                'status' => 'error',
+                'message' => 'Course not found'
+            ], 404);
+        }
+
+        $userId = $request->input('user_id');
+        $user = getUser($userId);
+        if ($user['status'] === 'error') {
+            return response()->json([
+                'status' => $user['status'],
+                'message' => $user['message'],
+            ], 404);
+        }
+
+        $isExistMyCourse = MyCourse::where('course_id', '=', $courseId)->where('user_id', '=', $userId)->exists();
+
+        if ($isExistMyCourse) {
+            return response()->json([
+                'status' => 'error',
+                'message' => 'user already taken this course'
+            ], 409);
+        }
+
+        $myCourse = MyCourse::create($data);
+        return response()->json([
+            'status' => 'success',
+            'data' => $myCourse
+        ]);
+    }
+}
