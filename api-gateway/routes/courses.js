3a7f1dd637433a00ec908c7f8a1c31744ff5d387
Agung Hasbi <agunghasbi13@gmail.com>
2023-09-10 20:16:26 +0700

    Integrasi API Courses dengan API GATEWAY

--- api-gateway/routes/courses.js
+++ api-gateway/routes/courses.js
@@ -1,12 +1,14 @@
 const express = require('express');
+const verifyToken = require('../middlewares/verifyToken');
 const router = express.Router();
 
 const coursesHandler = require('./handler/courses');
 
-// router.get('/', coursesHandler.getAll);
-// router.get('/:id', coursesHandler.get);
-router.post('/', coursesHandler.create);
-router.put('/:id', coursesHandler.update);
-router.delete('/:id', coursesHandler.destroy);
+router.get('/', coursesHandler.getAll);
+router.get('/:id', coursesHandler.get);
+
+router.post('/', verifyToken, coursesHandler.create);
+router.put('/:id', verifyToken, coursesHandler.update);
+router.delete('/:id', verifyToken, coursesHandler.destroy);
 
 module.exports = router;
